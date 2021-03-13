fa6179efdd08c07f82c6384fcbe87137ccc75244
Dom Hill <dominic.hilleb@hotmail.co.uk>
2021-03-13 17:42:19 +0000

    started gui - quick save

--- school/NEA/main.py
+++ school/NEA/main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import sqlite3
+import tkinter as tk
 # Connect to database and create table if needed
 db = sqlite3.connect("accounts.sqlite", detect_types=sqlite3.PARSE_DECLTYPES)
 db.execute("""CREATE TABLE  IF NOT EXISTS users (username TEXT,
@@ -18,7 +19,7 @@ class Player():
     """Class to represent a player
 
     Attributes:
-        username (str): username of user.
+        username (str): username of user
         _password (str): password of user
 
     Methods:
@@ -32,7 +33,7 @@ class Player():
         self._password = password
 
 
-    def searchUser(self):
+    def search(self):
         print(self.username, self._password)
         print('='*30)
         if self.username in [username[0] for username in db.execute("SELECT username FROM users")]:
@@ -40,18 +41,19 @@ class Player():
 
         else:
             print("username does not exist, Let's get you signed up")
-            self.registerUser()
+            self.register()
 
         for password in db.execute("SELECT password FROM users WHERE (username=?)", (self.username,)):
             password = password[0]
             # print(password)
             if self._password == password:
                 print("Login successful as {}".format(self.username))
+                return True
             else:
                 print("Incorrect password")
 
 
-    def registerUser(self):
+    def register(self):
         username = input("Enter new username: ")
         password = input("Enter new password: ")
         passwordConfirm = input("Confirm password")
@@ -59,7 +61,7 @@ class Player():
             db.execute("INSERT INTO users VALUES (?, ?)", (username, password))
             self.username = username
             self._password = password
-        self.searchUser()
+        self.search()
 
 
 def stats():
@@ -99,25 +101,87 @@ def stats():
     plt.show()
 
 
-# username = input("Enter username: ")
-# password = input("Enter password: ")
-username = 'dom'
-password = 'password'
-player1 = Player(username, password)
-# user.searchUser()
-username = 'mac'
-password = 'ok'
-player2 = Player(username, password)
-stats()
+class GUI(tk.Frame):
+    def __init__(self, master):
+        tk.Frame.__init__(self)
+        self.master = master
+        self.master.title("Dice game")
+        self.master.geometry('640x480-8-200')
+        self.frame = tk.Frame(self.master)
 
-# print(player1.__doc__)
 
-print(player1.username)
-print(player1._password)
+class UserAuthentification(GUI):
+    def __init__(self, master):
+        GUI.__init__(self, master)
+        playerText = tk.Variable(self.frame)
+        playerText.set("Sign in as player 1")
+        self.playerLabel = tk.Label(self.frame, textvariable=playerText)
+        self.playerLabel.config(font=("Courier", 30))
+        self.usernameLabel = tk.Label(self.frame, text='Username:')
+        self.usernameEntry1 = tk.Entry(self.frame, width=30)
+        self.passwordLabel = tk.Label(self.frame, text='Password:')
+        self.passwordEntry1 = tk.Entry(self.frame, width=30)
+        self.button1 = tk.Button(self.frame, text='Sign in',
+                                             width=25,
+                                             command=self.sign_in)
 
-print(player2.username)
-print(player2._password)
 
+        self.playerLabel.pack()
+        self.usernameLabel.pack()
+        self.usernameEntry1.pack()
+        self.passwordLabel.pack()
+        self.passwordEntry1.pack()
+        self.button1.pack(pady=8)
+        self.frame.pack()
+    def sign_in(self):
+        username = self.usernameEntry1.get()
+        password = self.passwordEntry1.get()
+        print(username, password)
+        player1 = Player(username, password)
+        if player1.search() == True:
+            self.newWindow = tk.Toplevel(self.master)
+            self.app = MainApplication(self.newWindow)
+
+
+
+class MainApplication(GUI):
+    def __init__(self, master):
+        GUI.__init__(self, master)
+        self.button1 = tk.Button(self.frame, text = 'Statistics', width = 25, command = self.show_statistics)
+        self.button1.pack()
+        self.frame.pack()
+    def show_statistics(self):
+        stats()
+
+
+
+
+if __name__ == '__main__':
+    # username = input("Enter username: ")
+    # password = input("Enter password: ")
+    # username = 'dom'
+    # password = 'password'
+    # player1 = Player(username, password)
+    # # user.search()
+    # username = 'mac'
+    # password = 'ok'
+    # player2 = Player(username, password)
+    # stats()
+
+    # print(player1.username)
+    # print(player1._password)
+    #
+    # print(player2.username)
+    # print(player2._password)
+
+    root = tk.Tk()
+    # print(root)
+    # app = GUI(root)
+    # root.mainloop()
+
+    # root2 = tk.Tk()
+    app = UserAuthentification(root)
+    root.mainloop()
 
 db.commit()
 db.close()
--- school/NEA/test.py
+++ school/NEA/test.py
@@ -1,15 +1,54 @@
-import sqlite3
-db = sqlite3.connect("accounts.sqlite", detect_types=sqlite3.PARSE_DECLTYPES)
+# import tkinter as tk
+#
+# class MainApplication(tk.Frame):
+#     def __init__(self, parent, *args, **kwargs):
+#         tk.Frame.__init__(self, parent, *args, **kwargs)
+#         self.parent = parent
+#
+#
+# if __name__ == "__main__":
+#     root = tk.Tk()
+#     MainApplication(root).pack(side="top", fill="both", expand=True)
+#     root.mainloop()
+
+import tkinter as tk
+
+class GUI(tk.Frame):
+    def __init__(self, master):
+        # tk.Frame.__init__(self, parent, *awgs, **kwargs)
+        tk.Frame.__init__(self)
+        self.master = master
+        self.master.title("Dice game")
+        self.master.geometry('640x480-8-200')
+        self.frame = tk.Frame(self.master)
+
+class userAuthentification(GUI):
+    def __init__(self, master):
+        # self.frame = tk.Frame(self.master)
+        GUI.__init__(self, master)
+        self.button1 = tk.Button(self.frame, text = 'New Window', width = 25, command = self.new_window)
+        self.button1.pack()
+        self.frame.pack()
+    def new_window(self):
+        self.newWindow = tk.Toplevel(self.master)
+        self.app = Demo2(self.newWindow)
 
 
+class Demo2:
+    def __init__(self, master):
+        self.master = master
+        self.frame = tk.Frame(self.master)
+        self.quitButton = tk.Button(self.frame, text = 'Quit', width = 25, command = self.close_windows)
+        self.quitButton.pack()
+        self.frame.pack()
+    def close_windows(self):
+        self.master.destroy()
 
 
+def main():
+    root = tk.Tk()
+    app = userAuthentification(root)
+    root.mainloop()
 
-# for passwordDb in db.execute("SELECT password FROM users WHERE (username=?)", (username,)):
-#     print(passwordDb[0])
-#     # if usernameDb == username and passwordDb == password:
-#     print("Login successful as {}".format(username))
-#     # else:
-#     #     print(username, password)
-for username in db.execute("SELECT username FROM users"):
-    print(username[0])
+if __name__ == '__main__':
+    main()
